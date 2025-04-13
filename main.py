b5c3a55f8392aac4cb5bb4e25b655fbd5f85db18
kylin <chqj98@gmail.com>
2025-04-14 00:37:00 +0800

    [dev] 4.14

---
+++ main.py
@@ -0,0 +1,68 @@
+import anyio
+import click
+import httpx
+import mcp.types as types
+from mcp.server.lowlevel import Server
+import sys
+from mcp.server.stdio import stdio_server
+from mcp.server.stdio import stdio_server
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+async def fetch_website(
+    url: str,
+) -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
+    headers = {
+        "User-Agent": "MCP Test Server (github.com/modelcontextprotocol/python-sdk)"
+    }
+    async with httpx.AsyncClient(follow_redirects=True, headers=headers) as client:
+        response = await client.get(url)
+        response.raise_for_status()
+        return [types.TextContent(type="text", text=response.text)]
+
+def main() -> int:
+    logger.info("---------------------------")
+    app = Server("mcp-website-fetcher")
+
+    @app.call_tool()
+    async def fetch_tool(
+        name: str, arguments: dict
+    ) -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
+        if name != "fetch":
+            raise ValueError(f"Unknown tool: {name}")
+        if "url" not in arguments:
+            raise ValueError("Missing required argument 'url'")
+        return await fetch_website(arguments["url"])
+
+    @app.list_tools()
+    async def list_tools() -> list[types.Tool]:
+        return [
+            types.Tool(
+                name="fetch",
+                description="Fetches a website and returns its content",
+                inputSchema={
+                    "type": "object",
+                    "required": ["url"],
+                    "properties": {
+                        "url": {
+                            "type": "string",
+                            "description": "URL to fetch",
+                        }
+                    },
+                },
+            )
+        ]
+
+
+    async def arun():
+        async with stdio_server() as streams:
+            await app.run(
+                streams[0], streams[1], app.create_initialization_options()
+            )
+
+    anyio.run(arun)
+
+
+main()
